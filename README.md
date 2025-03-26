c27d578147529c089d944385e60b4053ac2dcc63
pawitchaya <pawichaya.p@quadel.co.th>
2025-03-26 11:20:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# brick_breaker_deploy
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:brick_breaker_deploy/src/widgets/game_app.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GameApp());
+}
